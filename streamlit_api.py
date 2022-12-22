b620f33a9a6ed5dec28c3e42b5913a2b1ed502e1
emaddar <e.a.darwich@gmail.com>
2022-12-22 23:47:24 +0100

    now

--- streamlit_api.py
+++ streamlit_api.py
@@ -20,28 +20,16 @@ from millify import millify
 ##########################################
 
 
-st.set_page_config(page_icon=":movie_camera:",
+st.set_page_config(page_icon=":bar_chart:",
                 layout="wide")
 
 
 
 #########################################
 #########################################
-@st.cache(allow_output_mutation=True)
-def load_objects_par_regions():
-    objects_par_regions = pd.read_csv('CSV/objects_par_region.csv')
-    return objects_par_regions
+objects_par_regions = pd.read_csv('CSV/objects_par_region.csv')
+geo = json.load(open("GEOJSON/regions.geojson")) # lire les données
 
-
-@st.cache(allow_output_mutation=True)
-def load_geo():
-    geo = json.load(open("GEOJSON/regions.geojson")) # lire les données
-    return geo
-
-
-
-geo = load_geo()
-objects_par_regions = load_objects_par_regions()
 #########################################
 #########################################
 def make_grid(cols,rows):
@@ -69,31 +57,31 @@ with mygrid1[0][1]:
 
 En effet, chaque jour des dizaines d'objets sont perdus partout en France par les voyageurs, leur gestion est critique au niveau de la satisfaction client. Cependant le cout de leur gestion est critique également. On aimerait donc dimensionner au mieux le service en charge de les gérer mais pour cela il faut pouvoir anticiper de manière précise le volume d'objets perdus chaque jour. Votre manager a une intuition qu'il aimerait vérifier: plus il fait froid plus les voyageurs sont chargés (manteau, écharppes, gant) plus ils ont donc de probabilité de les oublier. Mais empiler toutes ces couches prend du temps, ce qui pousse aussi à se mettre en retard et dans la précipitation, à oublier d'autres affaires encore. A l'aide des données de la SNCF et d'autres données, essayez de creuser cette piste.
 
-++A partir de l’API open data de la sncf.++
+++A partir de l'API open data de la sncf.++
 
 Requeter la base de données des objets trouvés pour récupérer les données entre 2016 et 2021
-- Bonus: écrivez un brief permettant d’alimenter chaque jour votre BDD avec les nouvelles données.
+- Bonus: écrivez un brief permettant d'alimenter chaque jour votre BDD avec les nouvelles données.
 
-++A partir d’internet:++
+++A partir d'internet:++
 
 Récupérer la liste des températures journalières par ville en France entre 2016 et 2021.
 ++Data analyse VISUALISATION. - (Sur un streamlit)++
 
-Calculez entre 2016 et 2021 la somme du nombre d’objets perdus par semaine. Afficher sur un histogramme la répartition de ces valeurs. (un point correspond à une semaine dont la valeur est la somme).
+Calculez entre 2016 et 2021 la somme du nombre d'objets perdus par semaine. Afficher sur un histogramme la répartition de ces valeurs. (un point correspond à une semaine dont la valeur est la somme).
 
-Afficher l’évolution du nombre d’objets perdus à l’aide d’un plotly sur la période 2016-2021. On peut choisir d’afficher ou non certains types d’objet.
+Afficher l'évolution du nombre d'objets perdus à l'aide d'un plotly sur la période 2016-2021. On peut choisir d'afficher ou non certains types d'objet.
 
-Afficher une carte de France avec le nombre d’objets perdus en fonction de la fréquentation de voyageur de chaque région. Possibilité de faire varier par année et par type d’objets
+Afficher une carte de France avec le nombre d'objets perdus en fonction de la fréquentation de voyageur de chaque région. Possibilité de faire varier par année et par type d'objets
 
 ++Partie data analyse en vue de la DATA SCIENCE. - (sur un notebook)++
 
-Afficher le nombre d’objets perdus en fonction de la température sur un scatterplot Est ce que le nombre d’objets perdus est corrélé à la temperature?
+Afficher le nombre d'objets perdus en fonction de la température sur un scatterplot Est ce que le nombre d'objets perdus est corrélé à la temperature?
 
-Quelle est la médiane du nombre d’objets perdus en fonction de la saison?
+Quelle est la médiane du nombre d'objets perdus en fonction de la saison?
 
-Représenter cette information à l’aide d’un Boxplot. Est ce que le nombre d’objets perdus est corrélé à la saison?
+Représenter cette information à l'aide d'un Boxplot. Est ce que le nombre d'objets perdus est corrélé à la saison?
 
-Est ce que le type d’objet perdu est corrélé au mois?
+Est ce que le type d'objet perdu est corrélé au mois?
     """)
 
 
@@ -102,52 +90,69 @@ choice = st.selectbox('Search by :',('day', 'week', 'quarter', 'month', 'year'))
 
 
 
-@st.cache(allow_output_mutation=True)
-def load_df_weather_by_choice():
-    df_weather_by_choice = pd.read_csv(f"CSV/weather_by_dep_by_{choice}.csv")
-    return df_weather_by_choice
-df_weather_by_choice = load_df_weather_by_choice()
-
-
-
-
-@st.cache(allow_output_mutation=True)
-def load_df_choice_total():
-    df_choice_total = pd.read_csv(f'CSV/df_{choice}_total.csv')
-    return df_choice_total
-df_choice_total = load_df_choice_total()
-
-
-@st.cache(allow_output_mutation=True)
-def load_df_choice_by_type():
-    df_choice_by_type = pd.read_csv(f'CSV/df_{choice}_by_type.csv')
-    return df_choice_by_type
-df_choice_by_type = load_df_choice_by_type()
+df_weather_by_choice = pd.read_csv(f"CSV/weather_by_dep_by_{choice}.csv")
+df_choice_total = pd.read_csv(f'CSV/df_{choice}_total.csv')
+df_choice_by_type = pd.read_csv(f'CSV/df_{choice}_by_type.csv')
 
 if choice in ['day', 'week']:
-    @st.cache(allow_output_mutation=True)
-    def load_objet_region_by_choice():
-        objet_region_by_choice = pd.read_csv(f'CSV/objects_by_region_by_{choice}.csv')
-        return objet_region_by_choice
-    objet_region_by_choice = load_objet_region_by_choice()
-    
+    objet_region_by_choice = pd.read_csv(f'CSV/objects_by_region_by_{choice}.csv')
 else :
-    @st.cache(allow_output_mutation=True)
-    def load_objet_region_by_choice():
-        objet_region_by_choice = pd.read_csv(f'CSV/objects_by_region_by_{choice}.csv')
-        return objet_region_by_choice
-    objet_region_by_choice = load_objet_region_by_choice()
+    objet_region_by_choice = pd.read_csv(f'CSV/objects_by_region_by_{choice}.csv')
     
 
 mygrid2 = make_grid(1,6)
 with mygrid2[0][0]:
     st.metric("Lost items", millify(df_choice_total.nb_objets.sum(), precision=2))
+
+
 with mygrid2[0][1]:
     st.metric(f"Max lost items by {choice} :", millify(df_choice_total.sort_values('nb_objets', ascending=False).iloc[0,1], precision=2))
+with mygrid2[0][1]:
+    date = str(df_choice_total.sort_values('nb_objets', ascending=False).iloc[0,0]).split(' ',1)[0]
+    if choice == 'year':
+        st.write(f"In {choice} : {date.split('-', 2)[0]}")
+    elif choice == 'month':
+        st.write(f"In {choice} : {date.split('-', 2)[1]}")
+    else :
+        st.write(f"In {date}") 
+
+
 with mygrid2[0][2]:
-    st.metric(f"Max items lost in :", df_choice_total.sort_values('nb_objets', ascending=False).iloc[0,0])
+    st.metric(f"Min lost items by {choice} :", millify(df_choice_total.sort_values('nb_objets', ascending=True).iloc[0,1], precision=2))
+with mygrid2[0][2]:
+    date = str(df_choice_total.sort_values('nb_objets', ascending=True).iloc[0,0]).split(' ',1)[0]
+    if choice == 'year':
+        st.write(f"In {choice} : {date.split('-', 2)[0]}")
+    elif choice == 'month':
+        st.write(f"In {choice} : {date.split('-', 2)[1]}")
+    else :
+        st.write(f"In {date}") 
+
+with mygrid2[0][3]:
+    st.metric(f"Max temperature by {choice} :", str(round(df_weather_by_choice.sort_values('tc', ascending=False).iloc[0,5],2))+" c")
+with mygrid2[0][3]:
+    date = str(df_weather_by_choice.sort_values('tc', ascending=False).iloc[0,2]).split(' ',1)[0]
+    if choice == 'year':
+        st.write(f"In {choice} : {date.split('-', 2)[0]}")
+    elif choice == 'month':
+        st.write(f"In {choice} : {date.split('-', 2)[1]}")
+    else :
+        st.write(f"In {date}") 
+
+with mygrid2[0][4]:
+    st.metric(f"Min temperature by {choice} :", str(round(df_weather_by_choice.sort_values('tc', ascending=True).iloc[0,5],2))+" c")
+with mygrid2[0][4]:
+    date = str(df_weather_by_choice.sort_values('tc', ascending=True).iloc[0,2]).split(' ',1)[0]
+    if choice == 'year':
+        st.write(f"In {choice} : {date.split('-', 2)[0]}")
+    elif choice == 'month':
+        st.write(f"In {choice} : {date.split('-', 2)[1]}")
+    else :
+        st.write(f"In {date}") 
 
 
+with mygrid2[0][5]:
+    st.metric(f"Average temperature by {choice} :", str(round(df_weather_by_choice.tc.mean(),2))+" c")
 
 df_departements_weather_by_choice_lat_mean = df_weather_by_choice['lat'].mean()
 df_departements_weather_by_choice_long_mean = df_weather_by_choice['long'].mean()
@@ -237,35 +242,39 @@ HeatMap(df_departements_weather_by_choice,
 
 
 
-col1, col2 = st.columns(2)
-
-
-
-fig = go.Figure(data=[go.Histogram(nbinsx=20,x=df_choice_total.nb_objets)]).update_layout(
-    xaxis_title="N of found aobjects", yaxis_title=f"{choice} from 2016 to 2021", title=go.layout.Title(text=f"Sum of lost items per {choice}")
-)
-
-
-fig1 = go.Figure(data=[go.Line(x=df_choice_total.date, y = df_choice_total.nb_objets)]).update_layout(
-    xaxis_title=f"{choice} from 2016 to 2021", yaxis_title="N of found aobjects", title=go.layout.Title(text=f"Sum of lost items per {choice}")
-)
-
-with col1:
+with st.expander('\U0001F4CA Histogram and line plots graphics'):
     
-    st.plotly_chart(fig, use_container_width=True)
-    st_folium(hm, width=700, height=450)
-    
-
-
-with col2:
-    
-    st.plotly_chart(fig1, use_container_width=True)
-    st_folium(m, width=700, height=450)
-    if choice in ['day', 'week']:
-        st.warning('Attention : Brut values presented in this map', icon="⚠️")
+    col1, col2 = st.columns(2)
 
 
 
+    fig = go.Figure(data=[go.Histogram(nbinsx=20,x=df_choice_total.nb_objets)]).update_layout(
+        xaxis_title="N of found aobjects", yaxis_title=f"{choice} from 2016 to 2021", title=go.layout.Title(text=f"Sum of lost items per {choice}")
+    )
 
-fig2 = px.line(df_choice_by_type, x=df_choice_by_type.index, y='nb_objets', color='type', hover_name="type")
-st.plotly_chart(fig2, use_container_width=True)
+
+    fig1 = go.Figure(data=[go.Line(x=df_choice_total.date, y = df_choice_total.nb_objets)]).update_layout(
+        xaxis_title=f"{choice} from 2016 to 2021", yaxis_title="N of found aobjects", title=go.layout.Title(text=f"Sum of lost items per {choice}")
+    )
+    with col1:
+        
+        st.plotly_chart(fig, use_container_width=True)
+
+        
+    with col2:
+        
+        st.plotly_chart(fig1, use_container_width=True)
+
+    fig2 = px.line(df_choice_by_type, x=df_choice_by_type.index, y='nb_objets', color='type', hover_name="type")
+    st.plotly_chart(fig2, use_container_width=True)
+
+
+with st.expander('\U0001F30D Folium maps'):
+    col3, col4 = st.columns(2)
+    with col3:
+        st_folium(hm, width=700, height=450)
+
+    with col4:
+        st_folium(m, width=700, height=450)
+        if choice in ['day', 'week']:
+            st.warning('Attention : Brut values presented in this map')
